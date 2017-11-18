6c0aee073e285a57b5749b69e1c6cd438c5262ad
Nitin <getnpk@gmail.com>
2017-11-18 11:13:29 +0800

    Add quote, quote-grid components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="container">
+    <quote-grid :quotes=quotes></quote-grid>
   </div>
 </template>
 
 <script>
+import QuoteGrid from './components/QuoteGrid';
 export default {
+  data: function () {
+    return {
+      maxQuotes: 10,
+      quotes: ['Little by little one travels far ']
+    }
+  },
+  components: {
+    QuoteGrid
+  }
 }
 </script>
 
---
+++ src/components/Quote.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col-sm-6 col-md-4 col-md-3">
+    <div class="panel panel-defaut">
+      <div class="panel-body quote">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['quotes']
+  }
+</script>
+
+<style>
+  .panel-body {
+    font-family: cursive;
+    font-size: 24px;
+    color: lightseagreen;
+  }
+
+  .quote {
+    cursor: pointer;
+  }
+  .quote:hover {
+    background-color: lightcoral;
+  }
+</style>
